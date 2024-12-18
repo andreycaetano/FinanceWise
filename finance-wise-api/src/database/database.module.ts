a5ba872a5252b44dbae52e3c401447d818f851c1
andreycaetano <andreycaetano2102@gmail.com>
2024-12-18 14:56:15 -0300

    configurado typeorm

---
+++ finance-wise-api/src/database/database.module.ts
@@ -0,0 +1,21 @@
+import { Module } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRootAsync({
+      useFactory: async () => ({
+        type: 'postgres',
+        host: process.env.DB_HOST,
+        port: +process.env.DB_PORT,
+        username: process.env.DB_USERNAME,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+        autoLoadEntities: true,
+        synchronize: true
+      })
+    })
+  ],
+})
+export class DatabaseModule { }
